aa28d046a9f61d89048fc7ede7cd45ca084a38ac
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-09-03 10:37:48 -0300

    Added proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -13,5 +13,30 @@ class Blockchain:
         self.chain = []
         self.create_block(proof=1, previous_hash="0")
 
+    def create_block(self, proof, previous_hash):
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": str(datetime.datetime.now()),
+            "proof": proof,
+            "previous_hash": previous_hash,
+        }
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            calc = new_proof ** 2 - previous_proof ** 2
+            hash_operation = hashlib.sha256(str(calc).encode()).hexdigest()
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
 
 # Part 2 - Mining our Blockchain
