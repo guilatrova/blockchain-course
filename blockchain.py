ac1984e4ecc2b91261fab8c7bb51bf824fc7571e
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-09-06 13:56:55 -0300

    Added is_chain_valid and refactor

--- blockchain.py
+++ blockchain.py
@@ -17,7 +17,7 @@ class Blockchain:
         block = {
             "index": len(self.chain) + 1,
             "timestamp": str(datetime.datetime.now()),
-            "proof": proof,
+            "proof": proof,  # Nonce
             "previous_hash": previous_hash,
         }
         self.chain.append(block)
@@ -30,13 +30,47 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            calc = new_proof ** 2 - previous_proof ** 2
-            hash_operation = hashlib.sha256(str(calc).encode()).hexdigest()
+            hash_operation = self.calculate_hash_operation(new_proof, previous_proof)
             if hash_operation[:4] == "0000":
                 check_proof = True
             else:
                 new_proof += 1
         return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def calculate_hash_operation(self, new_proof, previous_proof):
+        calc = new_proof ** 2 - previous_proof ** 2
+        return hashlib.sha256(str(calc).encode()).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+
+        while len(chain) > block_index:
+            block = chain[block_index]
+
+            if not self._validate_block_hash(previous_block, block):
+                return False
+
+            if not self._validate_block_proof(previous_block, block):
+                return False
+
+            block_index += 1
+            previous_block = block
+
+        return True
+
+    def _validate_block_hash(self, prev, cur):
+        return cur["previous_hash"] != self.hash(prev)
+
+    def _validate_block_proof(self, prev, cur):
+        previous_proof = prev["proof"]
+        proof = cur["proof"]
+        hash_operation = self.calculate_hash_operation(proof, previous_proof)
+        return hash_operation[:4] != "0000"
 
 
 # Part 2 - Mining our Blockchain
