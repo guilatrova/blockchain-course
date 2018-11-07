5024491cbaa0dea8b5a1dec8ee347f2eb42f4e89
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-11-07 09:52:32 -0200

    Added functions for buy/sell and getters

--- smartcontract/latcoins_ico.sol
+++ smartcontract/latcoins_ico.sol
@@ -13,4 +13,27 @@ contract latcoin_ico {
         require (usd_invested * usd_to_latcoins + total_latcoins_bought <= max_latcoins);
         _;
     }
+
+    function equity_in_latcoins(address investor) external constant returns (uint) {
+        return equity_latcoins[investor];
+    }
+
+    function equity_in_usd(address investor) external constant returns (uint) {
+        return equity_usd[investor];
+    }
+
+    function buy_latcoins(address investor, uint usd_invested) external
+    can_buy_latcoins(usd_invested) {
+        uint latcoins_bought = usd_invested * usd_to_latcoins;
+
+        equity_latcoins[investor] += latcoins_bought;
+        equity_usd[investor] = equity_latcoins[investor] / usd_to_latcoins;
+        total_latcoins_bought += latcoins_bought;
+    }
+
+    function sell_latcoins(address investor, uint latcoins_sold) external {
+        equity_latcoins[investor] -= latcoins_sold;
+        equity_usd[investor] = equity_latcoins[investor] / usd_to_latcoins;
+        total_latcoins_bought -= latcoins_sold;
+    }
 }
